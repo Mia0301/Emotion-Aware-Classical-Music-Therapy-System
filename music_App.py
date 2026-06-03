e90bed0c2374cc84271ccde178be86c09b56c00f
Mia0301 <168523364+Mia0301@users.noreply.github.com>
2026-06-03 21:39:55 +0800

    Update music_App.py

--- music_App.py
+++ music_App.py
@@ -2,9 +2,33 @@ import streamlit as st
 import pandas as pd
 from music_recommendation import recommend_music
 from datetime import datetime
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
 
-model = load_model("emotion_model.keras")
+def build_model():
+    model = Sequential([
+        Conv2D(32, (3, 3), activation="relu", input_shape=(48, 48, 1)),
+        BatchNormalization(),
+        MaxPooling2D(2, 2),
+
+        Conv2D(64, (3, 3), activation="relu"),
+        BatchNormalization(),
+        MaxPooling2D(2, 2),
+
+        Conv2D(128, (3, 3), activation="relu"),
+        BatchNormalization(),
+        MaxPooling2D(2, 2),
+
+        Flatten(),
+        Dense(128, activation="relu"),
+        Dropout(0.5),
+        Dense(7, activation="softmax")
+    ])
+
+    model.load_weights("emotion_model.weights.h5")
+    return model
+
+model = build_model()
 
 st.set_page_config(
     page_title="Emotion-Aware Classical Music Therapy System",
